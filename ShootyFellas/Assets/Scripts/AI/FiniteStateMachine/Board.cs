88b950daac24a9df55eab516817a83149506e0ea
TiernanMcCarthy <tiernanmccarthy@outlook.com>
2021-02-28 13:37:56 +0000

    New Map

--- ShootyFellas/Assets/Scripts/AI/FiniteStateMachine/Board.cs
+++ ShootyFellas/Assets/Scripts/AI/FiniteStateMachine/Board.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class Board 
 {
-    public static float Width = 20;
+    public static float Width = 47;
 
-    public static float Height = 20;
+    public static float Height = 50;
 }
