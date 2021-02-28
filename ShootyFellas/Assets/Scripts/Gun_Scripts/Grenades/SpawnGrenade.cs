ae0023c1280b603696f2793b26e347e52effc508
Sam Hunt <up853184@myport.ac.uk>
2021-02-28 14:36:39 +0000

    awesome grenade moment
    XD

--- ShootyFellas/Assets/Scripts/Gun_Scripts/Grenades/SpawnGrenade.cs
+++ ShootyFellas/Assets/Scripts/Gun_Scripts/Grenades/SpawnGrenade.cs
@@ -6,15 +6,30 @@ public class SpawnGrenade : MonoBehaviour
 {
     public Hand handDirection;
     public GameObject defaultGrenade;
+
+    [SerializeField]
+    float lastGrenadeTime = 0;
+    [SerializeField]
+    float grenadeWaitTime;
+
+    public bool canThrow;
     void Start()
     {
-        
+        PrepareNextGrenade();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.G))
+        //if(Input.GetKeyDown(KeyCode.G))
+        //{
+        //    LaunchGrenade(defaultGrenade);
+        //}
+        if(Time.time - lastGrenadeTime > grenadeWaitTime)
+        {
+            canThrow = true;
+        }
+        if(canThrow)
         {
             LaunchGrenade(defaultGrenade);
         }
@@ -24,5 +39,14 @@ public class SpawnGrenade : MonoBehaviour
     {
         grenadeToSpawn.GetComponent<GrenadeMovement>().launchDirection = handDirection.directionFacing;
         Instantiate(grenadeToSpawn, transform.position, transform.rotation);
+        canThrow = false;
+        lastGrenadeTime = Time.time;
+        PrepareNextGrenade();
+    }
+
+    void PrepareNextGrenade()
+    {
+        grenadeWaitTime = Random.Range(10, 20);
+
     }
 }
---
+++ ShootyFellas/Assets/Scripts/PowerUps/GrenadeUpgrade.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeUpgrade : Base_Upgrade
+{
+    public SpawnGrenade spawnScript;
+
+    public List<GameObject> grenadeList;
+    void Awake()
+    {
+        grenadeList = new List<GameObject>();
+        spawnScript = GameObject.FindGameObjectWithTag("Player").GetComponent<SpawnGrenade>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            UpgradeGrenade();
+        }
+    }
+
+    public override void OnTouchPlayer()
+    {
+        UpgradeGrenade();
+    }
+    public void UpgradeGrenade()
+    {
+        int temp = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>().currentGrenade;
+        if (temp != 2)
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Character>().currentGrenade++;
+            spawnScript.defaultGrenade = grenadeList[temp];
+        }
+
+    }
+}
