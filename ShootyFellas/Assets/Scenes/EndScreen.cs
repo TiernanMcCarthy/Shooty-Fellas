7643fde3b541ce5a54eb3fd0471827e00b8d26f2
TiernanMcCarthy <tiernanmccarthy@outlook.com>
2021-02-28 22:35:37 +0000

    End Screen

---
+++ ShootyFellas/Assets/Scenes/EndScreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class EndScreen : MonoBehaviour
+{
+    public TextMeshProUGUI MattsMurdered;
+
+    public TextMeshProUGUI Profit;
+
+    public EnemySpawner ME;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ME = FindObjectOfType<EnemySpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ME.Score>0)
+        {
+            Profit.text = "Net Profit: " + ME.Score;
+            Profit.color = Color.green;
+        }
+        else if(ME.Score<0)
+        {
+            Profit.text = "Net Loss: " + ME.Score;
+            Profit.color = Color.red;
+        }
+
+        MattsMurdered.text = "Innocent Bystanders Harmed: " + ME.MattsKilled;
+    }
+
+    public void Leave()
+    {
+        Application.Quit();
+    }
+}
